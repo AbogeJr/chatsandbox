2fc8a78bcfc73c8732351e96e0acd86004b57d38
AbogeJr <junioraboge@gmail.com>
2024-08-04 06:37:01 +0300

    fix deepl error

--- src/pages/api/route.ts
+++ src/pages/api/route.ts
@@ -8,9 +8,9 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     if (req.method === 'POST') {
         const { text, target_lang } = req.body; 
         console.log("BODY", req.body);
-        const authKey = process.env.NEXT_PUBLIC_DEEPL_AUTH_KEY!; 
+        // const authKey = process.env.NEXT_PUBLIC_DEEPL_AUTH_KEY!; 
 
-        const translator = new deepl.Translator(authKey);
+        const translator = new deepl.Translator("c5157929-9815-4a46-b4c2-5de48ec5587e:fx");
         const result = await translator.translateText(text, null, target_lang)
         console.log( result);
         res.status(200).json({ text, target_lang, result });
